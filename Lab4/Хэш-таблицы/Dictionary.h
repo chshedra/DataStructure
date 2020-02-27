8f9a9e750e3bf83621bff76d75df9d5a0e4f5f4a
Chshedrin Andrey <shedrin2001@mail.ru>
2020-02-27 22:06:25 +0700

    Feature

--- Lab4/Хэш-таблицы/Dictionary.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Table.h"
-
-//TODO: Завести структуру словаря
-
-bool AddItem(Table** dictionary, string key, string value, int size, int &count);
-bool DeleteFromDictionary(Table** dictionary, string key, int size, int &count);
-void DisplayDictionary(Table** dictionary, int size);
